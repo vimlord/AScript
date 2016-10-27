76f41435e5ff52e5965b580dd5c66a4c42ec92a9
Christopher Hittner <chrishittner@gmail.com>
2016-10-26 21:40:51 -0400

    Token Header Comments

--- include/tokens.h
+++ include/tokens.h
@@ -5,8 +5,19 @@
 
 typedef void (*TokenProcess)(FILE*, char*, int);
 
+/**
+ * Handles the 'byte' keyword. This is used to instantiate a byte.
+ */
 void processByte(FILE* execfile, char* subline, int tokenid);
+
+/**
+ * Handles the 'if' and 'else' keywords. Used for if-else.
+ */
 void processIfElse(FILE* execfile, char* subline, int tokenid);
+
+/**
+ * Handles the 'while' keyword. Used for while loops.
+ */
 void processWhileLoop(FILE* execfile, char* subline, int tokenid);
 
 #endif
