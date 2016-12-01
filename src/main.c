fe902604dca3065c3bb5f36125a710a3d00b76e2
Christopher Hittner <chrishittner@gmail.com>
2016-12-01 15:26:58 -0500

    Multiple Source Files
    I have made it possible to compile from multiple files simultaneously. The
    files should be listed in the order in which they should be compiled. So,
    libraries used by everything should be added first, followed by others.
    Basically, dependencies should go after the files they depend on.

--- src/main.c
+++ src/main.c
@@ -12,9 +12,10 @@
 int main(int argc, char* argv[]) {
 
     //If no file is provided, immediately exit w/ an error message.
-    char* INPUTNAME = NULL;
     char* OUTPUTNAME = NULL;
     
+    List sourceFiles = makeList();
+
     //Parse through user arguments
     int i = 1;
     while(i < argc) {
@@ -34,82 +35,83 @@ int main(int argc, char* argv[]) {
                 else if(argv[i][j] == 'w')
                     addErrorFlags(1);
             }
-        } else
+        } else {
             //The value must be an output
-            INPUTNAME = argv[i];
+            char* filenm = (char*) malloc((strlen(argv[i])+1)*sizeof(char));
+            strcpy(filenm, argv[i]);
+            addToList(sourceFiles, filenm);
+        }
         
 
         i++;
     }
 
-    if(!INPUTNAME) {
+    if(!listSize(sourceFiles)) {
         //No input file.
         throwError("No input file specified.");
         return EINVAL;
     } else if(!OUTPUTNAME) {
-        //Gets the name of the output file, since none is specified.
-        i = 0;
-        while(INPUTNAME[i++] != '.');
-        //Make space and assign values
-        OUTPUTNAME = malloc((i+4) * sizeof(char));
-        i = 0;
-        while((OUTPUTNAME[i] = INPUTNAME[i]) != '.') i++;
-        //Sticks the file extension at the end
-        int j = 0;
-        for(j = 0; j <= 3; j++)
-            OUTPUTNAME[i+j+1] = "asm"[j];
+        OUTPUTNAME = malloc(8*sizeof(char));
+        strcpy(OUTPUTNAME, "run.asm");
     }
     
-    //Gets the file extensions for error checking
-    char* inputext = INPUTNAME;
     char* outputext = OUTPUTNAME;
-    while(*inputext && *inputext != '.')
-        inputext = &inputext[1];
-    while(*outputext && *outputext != '.')
-        outputext = &outputext[1];
     
-    if(strcmp(inputext, ".scr"))
-        throwError("Input file extension must be .scr");
-    else if(strcmp(outputext, ".asm"))
-        throwError("Output file extension must be .asm");
-    
-
-    //The source file
-    FILE* sourceFile = fopen(INPUTNAME, "r");
-    
-    //Checks to see if the file exists, and returns an error
-    //if the file does not exist.
-    if(!sourceFile)
-        throwError("Input file not found.");
-
     //Create a swap file for writing
     FILE* swapFile0 = fopen(".swapspace0.dta", "w+");
-    
-    //Moves the chars to the new file, without the newlines.
-    char* nextLine;
-    while(*(nextLine = stringUpTo(sourceFile, '\n', '\0', '\0')) != EOF) {
-        //Gets the length of the String.
-        int i = -1;
-        while(nextLine[++i]) {
-            //Handles commenting
-            if(nextLine[i] == '/' && nextLine[i+1] == '/') {
-                nextLine[i--] = '\0';
-            }
-        }
         
-        //printf("%s\n", nextLine);
-        fwrite(nextLine, 1, i, swapFile0);
+    i = 0;
+    while(i < listSize(sourceFiles)) {
+        //Gets the file extensions for error checking
+        char* INPUTNAME = (char*) getFromList(sourceFiles, i);
+        char* inputext = INPUTNAME;
 
-        //Needs to be freed
-        free(nextLine);
+        while(*inputext && *inputext != '.')
+            inputext = &inputext[1];
+        while(*outputext && *outputext != '.')
+            outputext = &outputext[1];
+        
+        if(strcmp(inputext, ".scr"))
+            throwError("Input file extension must be .scr");
+        else if(strcmp(outputext, ".asm"))
+            throwError("Output file extension must be .asm");
+
+        //The source file
+        FILE* sourceFile = fopen(INPUTNAME, "r");
+        
+        //Checks to see if the file exists, and returns an error
+        //if the file does not exist.
+        if(!sourceFile)
+            throwError("Input file not found.");
+        else
+            printf("Found input file '%s'\n", INPUTNAME);
+
+        //Moves the chars to the new file, without the newlines.
+        char* nextLine;
+        while(*(nextLine = stringUpTo(sourceFile, '\n', '\0', '\0')) != EOF) {
+            //Gets the length of the String.
+            int i = -1;
+            while(nextLine[++i]) {
+                //Handles commenting
+                if(nextLine[i] == '/' && nextLine[i+1] == '/') {
+                    nextLine[i--] = '\0';
+                }
+            }
+            
+            //printf("%s\n", nextLine);
+            fwrite(nextLine, 1, i, swapFile0);
+
+            //Needs to be freed
+            free(nextLine);
+        }
+
+        fclose(sourceFile); //Source file is no longer needed
+        
+        i++;
     }
-
     
-    //At this point, .swapspace0.swp contains 
-    //the source without newlines.
-    fclose(sourceFile); //Source file is no longer needed
     fclose(swapFile0);
-
+    
     //Reopen the swap file, only this time for reading.
     swapFile0 = fopen(".swapspace0.dta", "r");
 
@@ -133,7 +135,7 @@ int main(int argc, char* argv[]) {
      * Iterates through each line one by one. The use of parseSegment()
      * allows for lines containing code segments to be broken down.
      */
-    nextLine = getNextLine(swapFile0);
+    char* nextLine = getNextLine(swapFile0);
     while(*nextLine != '\0' && *nextLine != EOF) {
 
         //Parse the current line.
